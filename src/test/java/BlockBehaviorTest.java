2a860b2e49ea1066a9951a4e8c5bcdb70347b137
NguyenDuck <duck.duc.vit@gmail.com>
2024-10-12 20:37:54 +0700

    add stuffs

--- src/test/java/BlockBehaviorTest.java
+++ src/test/java/BlockBehaviorTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class BlockBehaviorTest {
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+public class BlockBehaviorTest {
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Test
+    void EmptyBlock() {
+        var x = new ExampleBlockBehavior();
+        logger.info(x.toString(4));
+    }
 }
--- src/test/java/ExampleBlockBehavior.java
+++ src/test/java/ExampleBlockBehavior.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class ExampleBlockBehavior {
+import io.vn.nguyenduck.auto_generate_api.SemVer;
+import io.vn.nguyenduck.auto_generate_api.block.BlockBehavior;
+
+public class ExampleBlockBehavior extends BlockBehavior {
+
+    public ExampleBlockBehavior() {
+        super(new SemVer(21), "example:unknown");
+    }
 }
